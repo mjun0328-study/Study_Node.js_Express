c0362595e498cdf3e350bceb68de3d7bd0ae3388
Kim Minjun <swmjoon@gmail.com>
2023-09-01 21:42:02 +0900

    변수

---
+++ syntax/variable2.js
@@ -0,0 +1,10 @@
+var name = "egoing";
+var letter =
+  "Dear " +
+  name +
+  " Aute ad id nostrud Lorem voluptate cupidatat culpa laborum. Excepteur " +
+  name +
+  " consectetur voluptate excepteur anim egoing reprehenderit non. Aliqua mollit anim esse aliquip cillum consequat cillum. Nulla fugiat laboris cupidatat officia consectetur sint sunt " +
+  name +
+  " occaecat.";
+console.log(letter);
