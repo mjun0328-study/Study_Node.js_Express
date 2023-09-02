4d649fa8c4f1e133cd1641e74c8b1efcd61d2a14
Kim Minjun <swmjoon@gmail.com>
2023-09-02 18:23:09 +0900

    글목록 출력하기

--- main.js
+++ main.js
@@ -9,34 +9,18 @@ var app = http.createServer(function (request, response) {
 
   if (pathname === "/") {
     if (queryData.id === undefined) {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      fs.readdir("./data/", function (error, filelist) {
+        console.log(filelist);
         var title = "Welcome";
         var description = "Hello, Node.js";
-        var template = `
-        <!doctype html>
-        <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
-          <h2>${title}</h2>
-          <p>${description}</p>
-        </body>
-        </html>
-      `;
-        response.writeHead(200);
-        response.end(template);
-      });
-    } else {
-      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
-        var title = queryData.id;
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
         var template = `
           <!doctype html>
           <html>
@@ -46,11 +30,7 @@ var app = http.createServer(function (request, response) {
           </head>
           <body>
             <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            ${list}
             <h2>${title}</h2>
             <p>${description}</p>
           </body>
@@ -58,6 +38,42 @@ var app = http.createServer(function (request, response) {
         `;
         response.writeHead(200);
         response.end(template);
+      });
+    } else {
+      fs.readdir("./data/", function (error, filelist) {
+        console.log(filelist);
+        var list = "<ul>";
+        var i = 0;
+        while (i < filelist.length) {
+          list =
+            list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+          i = i + 1;
+        }
+        list = list + "</ul>";
+        fs.readFile(
+          `data/${queryData.id}`,
+          "utf8",
+          function (err, description) {
+            var title = queryData.id;
+            var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+        `;
+            response.writeHead(200);
+            response.end(template);
+          }
+        );
       });
     }
   } else {
