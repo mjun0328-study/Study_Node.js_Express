639752e8b840a74d1bacdb75cad0557f1d81f578
Kim Minjun <swmjoon@gmail.com>
2023-09-04 01:26:43 +0900

    글수정 - 수정할 정보 전송

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ var app = http.createServer(function (request, response) {
         title,
         list,
         `
-          <form action="http://localhost:3000/create_process" method="post">
+          <form action="/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -113,6 +113,32 @@ var app = http.createServer(function (request, response) {
       fs.writeFile(`data/${title}`, description, "utf8", function (err) {
         response.writeHead(302, { Location: `/?id=${title}` });
         response.end("Success");
+      });
+    });
+  } else if (pathname === "/update") {
+    fs.readdir("./data/", function (error, filelist) {
+      var list = templateList(filelist);
+      fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+        var title = queryData.id;
+        var template = templateHTML(
+          title,
+          list,
+          `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `,
+          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        );
+        response.writeHead(200);
+        response.end(template);
       });
     });
   } else {
