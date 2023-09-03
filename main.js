2650c76ffb6883b40e613cbed31d05a6c7990e79
Kim Minjun <swmjoon@gmail.com>
2023-09-04 01:51:07 +0900

    글삭제 기능 완성

--- main.js
+++ main.js
@@ -70,7 +70,14 @@ var app = http.createServer(function (request, response) {
                 <h2>${title}</h2>
                 <p>${description}</p>  
               `,
-              `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+              `
+                <a href="/create">create</a>
+                <a href="/update?id=${title}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${title}">
+                  <input type="submit" value="delete">
+                </form>
+              `
             );
             response.writeHead(200);
             response.end(template);
@@ -154,8 +161,21 @@ var app = http.createServer(function (request, response) {
       fs.rename(`data/${id}`, `data/${title}`, function () {
         fs.writeFile(`data/${title}`, description, "utf8", function (err) {
           response.writeHead(302, { Location: `/?id=${title}` });
-          response.end("Success");
+          response.end();
         });
+      });
+    });
+  } else if (pathname === "/delete_process") {
+    var body = "";
+    request.on("data", function (data) {
+      body = body + data;
+    });
+    request.on("end", function () {
+      var post = qs.parse(body);
+      var id = post.id;
+      fs.unlink(`data/${id}`, function () {
+        response.writeHead(302, { Location: `/` });
+        response.end();
       });
     });
   } else {
