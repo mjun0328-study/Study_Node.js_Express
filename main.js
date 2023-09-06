0e74a0215066629c6e4da399d429760a3ef49a41
Kim Minjun <swmjoon@gmail.com>
2023-09-06 17:32:18 +0900

    라우터 - 파일로 분리

--- main.js
+++ main.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 const template = require("./lib/template.js");
-const path = require("path");
-const sanitizeHtml = require("sanitize-html");
-const qs = require("querystring");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const topicRoutes = require("./routes/topic");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,6 +16,8 @@ app.get("*", (req, res, next) => {
     next();
   });
 });
+
+app.use("/topic", topicRoutes);
 
 app.get("/", (req, res) => {
   var title = "Welcome";
@@ -36,110 +36,6 @@ app.get("/", (req, res) => {
   res.send(html);
 });
 
-app.get("/topic/create", (req, res) => {
-  var title = "WEB - create";
-  var list = template.list(req.list);
-  var html = template.HTML(
-    title,
-    list,
-    `
-        <form action="/topic/create_process" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-            <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-      `,
-    ""
-  );
-  res.send(html);
-});
-
-app.post("/topic/create_process", (req, res) => {
-  var post = req.body;
-  var filteredTitle = path.parse(post.title).base;
-  var description = post.description;
-  fs.writeFile(`data/${filteredTitle}`, description, "utf8", function (err) {
-    res.writeHead(302, { Location: `/topic/${filteredTitle}` });
-    res.end("Success");
-  });
-});
-
-app.get("/topic/update/:pageId", (req, res) => {
-  var filteredPath = path.parse(req.params.pageId).base;
-  var list = template.list(req.list);
-  fs.readFile(`data/${filteredPath}`, "utf8", function (err, description) {
-    var title = req.params.pageId;
-    var html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/topic/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/update?id=${title}">update</a>`
-    );
-    res.writeHead(200);
-    res.end(html);
-  });
-});
-
-app.post("/topic/update_process", (req, res) => {
-  var post = req.body;
-  var filteredId = path.parse(post.id).base;
-  var filteredTitle = path.parse(post.title).base;
-  var description = post.description;
-  fs.rename(`data/${filteredId}`, `data/${filteredTitle}`, function () {
-    fs.writeFile(`data/${filteredTitle}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${filteredTitle}`);
-    });
-  });
-});
-
-app.get("/topic/:pageId", (req, res, next) => {
-  var filteredPath = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredPath}`, "utf8", function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      var title = req.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      var list = template.list(req.list);
-      var html = template.HTML(
-        title,
-        list,
-        `
-          <h2>${sanitizedTitle}</h2>
-          <p>${sanitizedDescription}</p>
-        `,
-        `
-          <a href="/topic/create">create</a>
-          <a href="/topic/update/${sanitizedTitle}">update</a>
-          <form action="/topic/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>
-        `
-      );
-      res.send(html);
-    }
-  });
-});
-
 app.use((req, res, next) => {
   res.status(404).send("Sorry cant find that!");
 });
@@ -152,33 +48,3 @@ app.use((err, req, res, next) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-// var http = require("http");
-// var fs = require("fs");
-// var url = require("url");
-// var qs = require("querystring");
-// var path = require("path");
-// var sanitizeHtml = require("sanitize-html");
-
-// var template = require("./lib/template.js");
-
-// var app = http.createServer(function (request, response) {
-//   var _url = request.url;
-//   var queryData = url.parse(_url, true).query;
-//   var pathname = url.parse(_url, true).pathname;
-
-//   if (pathname === "/") {
-//     if (queryData.id === undefined) {
-//     } else {
-//     }
-//   } else if (pathname === "/create") {
-//   } else if (pathname === "/create_process") {
-//   } else if (pathname === "/update") {
-//   } else if (pathname === "/update_process") {
-//   } else if (pathname === "/delete_process") {
-//   } else {
-//     response.writeHead(404);
-//     response.end("Not Found");
-//   }
-// });
-// app.listen(3000);
