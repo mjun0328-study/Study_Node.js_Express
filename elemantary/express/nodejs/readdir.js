9995944b9ed1c8634f320ce18f183c74013dd84a
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-09-24 23:49:32 +0900

    === 파일 정리 ===

--- elemantary/express/nodejs/readdir.js
+++
@@ -1,6 +0,0 @@
-var testfolder = "./data/";
-var fs = require("fs");
-
-fs.readdir(testfolder, function (error, filelist) {
-  console.log(filelist);
-});
--- elemantary/express/syntax/conditional.js
+++
@@ -1,8 +0,0 @@
-var args = process.argv;
-console.log(args);
-if (args[2] === "1") {
-  console.log("A");
-} else {
-  console.log("B");
-}
-console.log("C");
