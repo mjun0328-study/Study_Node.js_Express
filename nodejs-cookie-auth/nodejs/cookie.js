8c54ca0de5a6ac9ce0d5c5270378cd201d137b48
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-09-25 04:06:07 +0900

    쿠키의 읽기

--- nodejs-cookie-auth/nodejs/cookie.js
+++ nodejs-cookie-auth/nodejs/cookie.js
@@ -1,6 +1,13 @@
 var http = require("http");
+var cookie = require("cookie");
+
 http
   .createServer(function (request, response) {
+    var cookies = {};
+    if (request.headers.cookie !== undefined) {
+      cookies = cookie.parse(request.headers.cookie);
+    }
+    console.log(cookies);
     // response.writeHead(200, {
     //   "Set-Cookie": ["hello=world", "welcome_to=nodejs"],
     // });
