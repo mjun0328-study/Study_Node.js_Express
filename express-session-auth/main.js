664dfb6c586c0dd98fca6726264b94db56c965bc
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-09-25 23:38:13 +0900

    인증구현 - UI 만들기

--- express-session-auth/main.js
+++ express-session-auth/main.js
@@ -1,36 +1,38 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var fs = require('fs');
-var bodyParser = require('body-parser');
-var compression = require('compression');
-var helmet = require('helmet')
+var fs = require("fs");
+var bodyParser = require("body-parser");
+var compression = require("compression");
+var helmet = require("helmet");
 app.use(helmet());
 
-var indexRouter = require('./routes/index');
-var topicRouter = require('./routes/topic');
+var indexRouter = require("./routes/index");
+var topicRouter = require("./routes/topic");
+var authRouter = require("./routes/auth");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+app.get("*", function (request, response, next) {
+  fs.readdir("./data", function (error, filelist) {
     request.list = filelist;
     next();
   });
 });
 
-app.use('/', indexRouter);
-app.use('/topic', topicRouter);
+app.use("/", indexRouter);
+app.use("/topic", topicRouter);
+app.use("/auth", authRouter);
 
-app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry cant find that!");
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
 });
